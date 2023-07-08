171a6518e5789536692c6581683493cac933f230
TalysonSoares <talysonsoares40@gmail.com>
2023-07-08 15:27:27 -0300

    Criando footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/home/Home';
 
 //components
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
             <Route path='/' element={<Home />}/>
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   )
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return <footer>
+        <h3>Viva sua liberdade sobre duas rodas!</h3>
+        <p>Motoverse &copy; 2023</p>
+  </footer>
+}
+
+export default Footer
