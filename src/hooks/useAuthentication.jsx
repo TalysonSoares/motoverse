bc3c289be59d0566c2036cd51b6de454a55b5573
TalysonSoares <talysonsoares40@gmail.com>
2023-07-09 14:56:50 -0300

    Criando hook de autenticação

---
+++ src/hooks/useAuthentication.jsx
@@ -0,0 +1,21 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, uptadeProfile, signOut } from 'firebase/auth'
+
+import { useState, useEffect } from 'react';
+
+export const useAuthentication = () => {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    // cleanup
+    // deal with memory leak
+    const [cancelled, setCancelled] = useState(false);
+
+    const auth = getAuth();
+
+    function checkIfIsCancelled() {
+        if(cancelled) {
+            return;
+        }
+    }
+
+}
