73e90b87c00c4e7a5755442f74ba6ce7e731d1fb
TalysonSoares <talysonsoares40@gmail.com>
2023-07-14 03:50:11 -0300

    css da home

---
+++ src/components/productDetails.jsx/ProductDetails.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+import styles from "./ProductDetails.module.css";
+
+const ProductDetails = ({ product }) => {
+    return (
+        <>
+            <div className={styles.card}>
+                <img className={styles.image} src={product.image} alt={product.title} />
+                <div className={styles.information}>
+                    <p>{product.title}</p>
+                    <span>R$ {product.price}</span>
+                <Link to={`/prooducts/${product.id}`} className={styles.buy}><span>Comprar</span></Link>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default ProductDetails
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,24 @@
+import ProductDetails from "../../components/productDetails.jsx/ProductDetails";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments"
 import styles from "./Home.module.css"
 
 const Home = () => {
   const {documents: products, loading} = useFetchDocuments("products");
-  console.log(products)
   return (
     <div className={styles.home}>
-      <h1>Home</h1>
-      {products && products.map((product) => (
-        <h1 key={product.id}>{product.title}</h1>
-      ))}
       
+      <h1>Home</h1>
+      <div className={styles.products}>
+        {products && products.map((product) => <ProductDetails key={product.id} product={product}/>)}
+        <div className={styles.card}>
+          <div className={styles.image}></div>
+          <h1>Harley-Davidson</h1>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.image}></div>
+          <h1>Harley-Davidson</h1>
+        </div>
+      </div>
     </div>
   )
 }
